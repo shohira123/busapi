801adae03cd0b7240af37dd9f43c8e93a84a14be
shohira123 <shohira95@gmail.com>
2025-03-16 23:49:12 +0530

    changes in connection issue fix

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
 // const mongoose = require("mongoose");
 // // const uri="mongodb+srv://shohira95:Z8yONTn1vl6FCJYu@mycluster.3vfxa.mongodb.net/?retryWrites=true&w=majority&appName=mycluster"
 // // const uri="mongodb://localhost:27017/db"
@@ -14,22 +16,21 @@
 // }
 // module.exports = connectdb;
 
-const mongoose = require("mongoose");
-require("dotenv").config();
+
 // const mongodb_URL = "mongodb://localhost:27017/busdb";
 // const mongodb_URL = "mongodb://0.0.0.0:27017/busdb";
-// mongoose.connect("mongodb://localhost:27017/studentdb",{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>{
-// const MONGODB_URI = process.env.MONGODB_URI;
-// mongoose.connect(MONGODB_URI).then(() => {
+// mongoose.connect("mongodb://localhost:27017/studentdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI).then(() => {
 
-//     console.log("connected");
-// }).catch((e) => {
-//     console.log(e);
-// })
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Reduce timeout to 5s
+    console.log("connected");
+}).catch((e) => {
+    console.log("issue");
 })
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.error("MongoDB Connection Error:", err));
+// mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     serverSelectionTimeoutMS: 5000, // Reduce timeout to 5s
+// })
+//     .then(() => console.log("MongoDB Connected"))
+//     .catch(err => console.error("MongoDB Connection Error:", err));
