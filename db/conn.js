ef9456bcef54326e3604396c4ed587934fa39c08
shohira123 <shohira95@gmail.com>
2025-03-16 23:21:38 +0530

    changes in connection issue fix

--- db/conn.js
+++ db/conn.js
@@ -19,10 +19,17 @@ require("dotenv").config();
 // const mongodb_URL = "mongodb://localhost:27017/busdb";
 // const mongodb_URL = "mongodb://0.0.0.0:27017/busdb";
 // mongoose.connect("mongodb://localhost:27017/studentdb",{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>{
-const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI).then(() => {
+// const MONGODB_URI = process.env.MONGODB_URI;
+// mongoose.connect(MONGODB_URI).then(() => {
 
-    console.log("connected");
-}).catch((e) => {
-    console.log(e);
+//     console.log("connected");
+// }).catch((e) => {
+//     console.log(e);
+// })
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Reduce timeout to 5s
 })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
