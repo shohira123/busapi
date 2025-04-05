d749f24ab6e980d040c6e4d115e9fca6dce373d5
shohira123 <shohira95@gmail.com>
2025-04-06 02:26:28 +0530

    location problem solve

--- controller/useraccountController.js
+++ controller/useraccountController.js
@@ -57,16 +57,16 @@ const useraccount = async (req, res) => {
                     as: "to_location_info" // new field with to location details
                 }
             },
-            {
-                $project: {
-                    _id: 1,  // Include the booking model _id
-                    scheduleDetails: "$location_info", // Include location_info from postbusschedules
-                    fromLocationName: "$from_location_info",
-                    // fromLocationName: { $arrayElemAt: ["$from_location_info.Location", 0] }, // Get the location name from from_location_info
-                    toLocationName: { $arrayElemAt: ["$to_location_info.Location", 0] }, // Get the location name from to_location_info
-                    // Add any other fields you need from the booking model here
-                }
-            }
+            // {
+            //     $project: {
+            //         _id: 1,  // Include the booking model _id
+            //         scheduleDetails: "$location_info", // Include location_info from postbusschedules
+            //         fromLocationName: "$from_location_info.Location",
+            //         // fromLocationName: { $arrayElemAt: ["$from_location_info.Location", 0] }, // Get the location name from from_location_info
+            //         toLocationName: { $arrayElemAt: ["$to_location_info.Location", 0] }, // Get the location name from to_location_info
+            //         // Add any other fields you need from the booking model here
+            //     }
+            // }
         ]);
 
         if (mydata) {
