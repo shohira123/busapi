a3e5dd0f2602ffd2654ee71297219f329af9585b
shohira123 <shohira95@gmail.com>
2025-04-10 20:59:23 +0530

    admin login

--- controller/adminController.js
+++ controller/adminController.js
@@ -4,7 +4,16 @@ const adminlogin_model = require('../model/adminlogin');
 const adminlogin = async (req, res) => {
     try {
         console.log("adminlogin ");
-        console.log(req.params);
+        const emailuser = req.body.email;
+        const passuser = req.body.password;
+        const mydata = await adminlogin_model.findOne({ "email": emailuser, "password": passuser })
+        if (mydata === undefined || mydata === null) {
+            res.status(401).json({ msg: "user not match", result: false })
+        }
+        else {
+            res.status(200).send({ msg: "User login successfully", result: true, data: mydata })
+            // res.status(200).send({})
+        }
     }
     catch (error) {
         console.log(error);
