37ad21536ae8fdaec7b9e5238d9fafe09a486eb6
shohira123 <shohira95@gmail.com>
2025-04-10 20:27:17 +0530

    admin login

--- routes/adminroute.js
+++ routes/adminroute.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const { adminController } = require("../controller/adminController");
 
-router.route("/login").post("adminController");
+router.route("/login").post(adminController);
 module.exports = router;
